18a2aa88884049bbafa923e37e5d60021ccaf1e9
jo <ljonas@riseup.net>
2023-02-11 01:51:53 +0100

    chore(web): setup i18n

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createI18n } from 'vue-i18n'
+import messages from '@intlify/unplugin-vue-i18n/messages'
 
 import App from './App.vue'
 
@@ -8,5 +10,12 @@ import './assets/main.css'
 const app = createApp(App)
 
 app.use(createPinia())
+
+app.use(
+  createI18n({
+    locale: 'en',
+    messages
+  })
+)
 
 app.mount('#app')
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -2,10 +2,18 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+
+    // https://github.com/intlify/bundle-tools/tree/main/packages/unplugin-vue-i18n
+    VueI18nPlugin({
+      include: ['./src/locales/*.json']
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
